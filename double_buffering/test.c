0079fd1f77d742b3b1a48fb65237bc97b521184b
Manuele Rusci <manuele.rusci@unibo.it>
2021-03-23 12:52:09 +0100

    some cleaning to double buffering test code

--- double_buffering/test.c
+++ double_buffering/test.c
@@ -11,22 +11,32 @@
  */
 
 #include "pmsis.h"
-#include <bsp/bsp.h>
+#include <bsp/bsp.h>    // including bps for interfacing with the RAM external devices
 
 //#define PRINT_MATRIX
 #define CHECK_RESULTS
-#define N_WORD 500
-#define N_BYTE (N_WORD*sizeof(int))
+#define N               (512)
+#define N_BYTE          (N*sizeof(int))
 
-// global variables
-int A[N_WORD];
-int B[N_WORD];
+/***
+     Global Variables 
+***/
+// operand inputs
+PI_L2 int A[N];   
+PI_L2 int B[N];
+// global struct define
+static struct pi_device ram;
+static struct pi_hyperram_conf conf;
+// output operand on external memory
+static uint32_t C_ptr_L3; 
 
+
+// additional variables for L3 buffering
 #ifndef DOUBLE_BUFFERING
-    int tempC[N_WORD];
+    int tempC[N];
 #else
-    int tempC[2*N_WORD];
-    static struct pi_task ram_write_tasks[N_WORD];
+    int tempC[2*N];
+    static struct pi_task ram_write_tasks[N];
     static int ram_returns = 0;
     // Callback for asynchronous ram write
     static void end_of_tx(void *arg)
@@ -36,40 +46,37 @@ int B[N_WORD];
     }
 #endif
 
-char memC[N_BYTE];
-static uint32_t hyper_buff; // result on L3
-
-// global struct define
-static struct pi_device ram;
-static struct pi_hyperram_conf conf;
-
+// need for verification
+#ifdef CHECK_RESULTS
+    char memC[N_BYTE];
+#endif
 
 // matrix functions
-void task_initMat()
+void task_initVec(int * vec, int dim)
 {
-    for(int i=0;i<N_WORD;i++){
-        A[i] = i;
-        B[i] = i;
+    for(int i=0; i<dim; i++){
+        vec[i] = i;
     }
 }
 
-void task_VectProdScalar(int scalarA, int* matB, int * matC, int dim)
-{
-    for(int i=0;i<dim;i++){
-        matC[i] = scalarA * matB[i];
-    }
-}
-
-void print_matrix(int * mat, int dim)
+// micro kernel task
+void task_VectProdScalar(int scalarA, int* vecB, int * vecC, int dim)
 {
     for(int i=0;i<dim;i++){
-        for(int j=0;j<dim;j++){
-            printf("%02d ", mat[i*dim+j]);
-        }
-        printf("\n");
+        vecC[i] = scalarA * vecB[i];
     }
 }
 
+//utils
+void print_vector(int * vec, int dim)
+{
+    for(int i=0; i<dim; i++){
+        printf("%02d ", vec[i]);
+    }
+    printf("\n");
+}
+
+// main function
 int main()
 {
     // Init & open external device ram
@@ -82,20 +89,21 @@ int main()
     }
 
     // Allocate output buffer
-    if (pi_ram_alloc(&ram, &hyper_buff, N_BYTE*N_BYTE))
+    if (pi_ram_alloc(&ram, &C_ptr_L3, N_BYTE*N_BYTE))
     {
         printf("Ram malloc failed !\n");
         pmsis_exit(-4);
     }
 
-    // initialize matrix operands
-    task_initMat();
+    // initialize input vector operands
+    task_initVec(A, N);
+    task_initVec(B, N);
 
 #ifdef PRINT_MATRIX
-    printf("\n\nThis is the Matrix A\n");
-    print_matrix(A, N_WORD);
-    printf("\n\nThis is the Matrix B\n");
-    print_matrix(B, N_WORD);
+    printf("\n\nThis is the Vector A\n");
+    print_vector(A, N);
+    printf("\n\nThis is the Vector B\n");
+    print_vector(B, N);
 #endif
     //initialize performance counters
     pi_perf_conf(
@@ -110,25 +118,25 @@ int main()
 
     // task to measure
 #ifndef DOUBLE_BUFFERING
-    for(int i=0; i<N_WORD;i++){
-        task_VectProdScalar(A[i], B, tempC, N_WORD);
-        pi_ram_write(&ram, hyper_buff+i*N_BYTE, tempC, (uint32_t) N_BYTE);
+    for(int i=0; i<N;i++){
+        task_VectProdScalar(A[i], B, tempC, N);
+        pi_ram_write(&ram, C_ptr_L3+i*N_BYTE, tempC, (uint32_t) N_BYTE);
     }
 #else
     int i_curr=1;
     int i_prev=0;
     int buffer_id;
-    task_VectProdScalar(A[0], B, tempC, N_WORD);
-    for(i_curr; i_curr<N_WORD;i_curr++){
+    task_VectProdScalar(A[0], B, tempC, N);
+    for(i_curr; i_curr<N;i_curr++){
         buffer_id = i_curr & 0x1;
-        pi_ram_write_async(&ram, hyper_buff+i_prev*N_BYTE, &tempC[N_WORD*(1-buffer_id)], (uint32_t) N_BYTE, pi_task_callback(&ram_write_tasks[i_prev], end_of_tx, NULL));
-        task_VectProdScalar(A[i_curr], B, &tempC[N_WORD*buffer_id], N_WORD);
+        pi_ram_write_async(&ram, C_ptr_L3+i_prev*N_BYTE, &tempC[N*(1-buffer_id)], (uint32_t) N_BYTE, pi_task_callback(&ram_write_tasks[i_prev], end_of_tx, NULL));
+        task_VectProdScalar(A[i_curr], B, &tempC[N*buffer_id], N);
         i_prev++;
         while(ram_returns != i_curr) {
             pi_yield();
         }
     }
-    pi_ram_write_async(&ram, hyper_buff+i_prev*N_BYTE, &tempC[N_WORD*buffer_id], (uint32_t) N_BYTE, pi_task_callback(&ram_write_tasks[i_prev], end_of_tx, NULL));    // last transfer
+    pi_ram_write_async(&ram, C_ptr_L3+i_prev*N_BYTE, &tempC[N*buffer_id], (uint32_t) N_BYTE, pi_task_callback(&ram_write_tasks[i_prev], end_of_tx, NULL));    // last transfer
 
     while(ram_returns != i_curr) {
         pi_yield();
@@ -146,10 +154,10 @@ int main()
 
 #ifdef CHECK_RESULTS
     // read back and check results
-    for(int i=0; i<N_WORD;i++){
-        pi_ram_read(&ram, hyper_buff+i*N_BYTE, memC, (uint32_t) N_BYTE);
-        task_VectProdScalar(A[i], B, tempC, N_WORD);
-        for(int j=0; j<N_WORD;j++){
+    for(int i=0; i<N;i++){
+        pi_ram_read(&ram, C_ptr_L3+i*N_BYTE, memC, (uint32_t) N_BYTE);
+        task_VectProdScalar(A[i], B, tempC, N);
+        for(int j=0; j<N;j++){
             if(tempC[j] != *((int*) memC+j)){
                 printf("Error, row: %d, index: %d, expected: 0x%x, read: 0x%x\n", i, j, tempC[j], *((int*) memC+j));
                 pmsis_exit(-5);
@@ -160,7 +168,7 @@ int main()
     printf("Test success\n");
 #endif
 
-    pi_ram_free(&ram, hyper_buff, N_BYTE*N_BYTE);
+    pi_ram_free(&ram, C_ptr_L3, N_BYTE*N_BYTE);
     pi_ram_close(&ram);
 
     pmsis_exit(0);
